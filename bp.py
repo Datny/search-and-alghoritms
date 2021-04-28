0c4a6b4e866bcc6c40adbd55510e1e00aca7d268
Datny <datny@datny.datny>
2021-04-28 14:59:02 -0700

    way to communicate with terminal stdin and stout using python

---
+++ bp.py
@@ -0,0 +1,6 @@
+from util import Node, QueueFrontier
+
+# Solving problem know as Knapsack problem
+# Greedy Knapsack Heuristic with sanity check
+# If backpack can hold weight 1000  and we have item1 = {weight:1000, volume: 1000}, item2= {weight=1, value=2}
+#  weight/volume is better for item 2 but we do sanity check with only values of items 
